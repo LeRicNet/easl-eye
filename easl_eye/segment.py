afb9a4c43f82f00564488a212c7658481fd2d82f
lericnet <ericprince82@gmail.com>
2024-06-21 17:41:58 +0000

    add basic MedSAM segment module; update viz.py

---
+++ easl_eye/segment.py
@@ -0,0 +1,70 @@
+import torch
+from segment_anything import sam_model_registry
+from skimage import io, transform
+import torch.nn.functional as F
+import pandas as pd
+import cv2
+
+
+def init_medsam():
+    #%% load model and image
+    MedSAM_CKPT_PATH = "/home/jovyan/medsam_vit_b.pth"
+    device = "cpu"
+    medsam_model = sam_model_registry['vit_b'](checkpoint=MedSAM_CKPT_PATH)
+    medsam_model = medsam_model.to(device)
+    medsam_model.eval()
+    
+    img_np = io.imread('/home/jovyan//img_demo.png')
+    if len(img_np.shape) == 2:
+        img_3c = np.repeat(img_np[:, :, None], 3, axis=-1)
+    else:
+        img_3c = img_np
+    H, W, _ = img_3c.shape
+
+def segment_image(img_3c, medsam_model):
+    #%% image preprocessing and model inference
+    img_1024 = transform.resize(img_3c, (1024, 1024), order=3, preserve_range=True, anti_aliasing=True).astype(np.uint8)
+    img_1024 = (img_1024 - img_1024.min()) / np.clip(
+        img_1024.max() - img_1024.min(), a_min=1e-8, a_max=None
+    )  # normalize to [0, 1], (H, W, 3)
+    # convert the shape to (3, H, W)
+    img_1024_tensor = torch.tensor(img_1024).float().permute(2, 0, 1).unsqueeze(0).to(device)
+    
+    box_np = np.array([[95,255, 190, 350]])
+    # transfer box_np t0 1024x1024 scale
+    box_1024 = box_np / np.array([W, H, W, H]) * 1024
+    with torch.no_grad():
+        image_embedding = medsam_model.image_encoder(img_1024_tensor) # (1, 256, 64, 64)
+    
+    medsam_seg = medsam_inference(medsam_model, image_embedding, box_1024, H, W)
+
+@torch.no_grad()
+def medsam_inference(medsam_model, img_embed, box_1024, H, W):
+    box_torch = torch.as_tensor(box_1024, dtype=torch.float, device=img_embed.device)
+    if len(box_torch.shape) == 2:
+        box_torch = box_torch[:, None, :] # (B, 1, 4)
+
+    sparse_embeddings, dense_embeddings = medsam_model.prompt_encoder(
+        points=None,
+        boxes=box_torch,
+        masks=None,
+    )
+    low_res_logits, _ = medsam_model.mask_decoder(
+        image_embeddings=img_embed, # (B, 256, 64, 64)
+        image_pe=medsam_model.prompt_encoder.get_dense_pe(), # (1, 256, 64, 64)
+        sparse_prompt_embeddings=sparse_embeddings, # (B, 2, 256)
+        dense_prompt_embeddings=dense_embeddings, # (B, 256, 64, 64)
+        multimask_output=False,
+        )
+
+    low_res_pred = torch.sigmoid(low_res_logits)  # (1, 1, 256, 256)
+
+    low_res_pred = F.interpolate(
+        low_res_pred,
+        size=(H, W),
+        mode="bilinear",
+        align_corners=False,
+    )  # (1, 1, gt.shape)
+    low_res_pred = low_res_pred.squeeze().cpu().numpy()  # (256, 256)
+    medsam_seg = (low_res_pred > 0.5).astype(np.uint8)
+    return medsam_seg
--- easl_eye/viz.py
+++ easl_eye/viz.py
@@ -1,7 +1,13 @@
+# %% environment and functions
+import numpy as np
 import matplotlib.pyplot as plt
+import os
+join = os.path.join
+
+from skimage import io, transform
+
 import pandas as pd
 import cv2
-import numpy as np
 import easl_eye as eye
 
 def raw_gaze_plot(data):
@@ -33,40 +39,31 @@ def session_video(aligned_data, instance_map, dims: tuple=(900, 900), return_arr
 	else:
 		# Define the codec using VideoWriter_fourcc() and create a VideoWriter object
 		fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-		video = cv2.VideoWriter('output.mp4', fourcc, 55.0, dims, isColor=False)
+		video = cv2.VideoWriter('/home/jovyan/output.mp4', fourcc, 250, dims, isColor=False)
 
 		## Write the frames
 		for i, image in enumerate(images):
 			image = cv2.resize(image, dims)
-			
-			if overlay_eyetracking:
-			
-				if aligned_data.in_roi_filter_x[i]:
-
-					# Create a scatter plot
-					fig, ax = plt.subplots()
-					ax.scatter(3, 1, 2, 'red')
-					# ax.scatter(*zip(*points))
-					plt.axis('off')
-
-					# Convert the plot to a cv2 image
-					fig.canvas.draw()
-					plot_img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-					plot_img = plot_img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-					plot_img = cv2.cvtColor(plot_img,cv2.COLOR_RGB2BGR)
-
-					# Overlay the scatter plot on the image
-					overlay_image = cv2.addWeighted(image, 0.7, plot_img, 0.3, 0)
-
-					video.write(overlay_image)
-
-					# Close the plot to free up memory
-					plt.close(fig)
-
-				else:
-					video.write(image)
-			else:
+			if aligned_data.in_roi_filter_x[i]:
 				video.write(image)
 				
 		# Finally release the video
 		video.release()
+
+# visualization functions
+# source: https://github.com/facebookresearch/segment-anything/blob/main/notebooks/predictor_example.ipynb
+# change color to avoid red and green
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([251/255, 252/255, 30/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='blue', facecolor=(0,0,0,0), lw=2))
+
